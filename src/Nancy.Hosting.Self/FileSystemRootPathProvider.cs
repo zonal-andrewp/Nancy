602fe620f8fa1d4b14132f242c8347c864d4b07a
Jonathan Channon <jonathan.channon@gmail.com>
2015-05-15 10:24:39 +0100

    nancy demo working with added ifdef for dnx451 in defaultrootpathprovider and filesystemrootpathpaorvider

--- src/Nancy.Hosting.Self/FileSystemRootPathProvider.cs
+++ src/Nancy.Hosting.Self/FileSystemRootPathProvider.cs
@@ -1,17 +1,34 @@
 namespace Nancy.Hosting.Self
 {
-    using System.IO;
-    using System.Reflection;
+#if DNX451
+    using Microsoft.Framework.Runtime;
+    using Microsoft.Framework.Runtime.Infrastructure;
+#endif
 
     public class FileSystemRootPathProvider : IRootPathProvider
     {
+        private readonly IApplicationEnvironment env;
+
+        public FileSystemRootPathProvider()
+        {
+#if DNX451
+            this.env = CallContextServiceLocator.Locator.ServiceProvider.GetService(typeof(IApplicationEnvironment)) as IApplicationEnvironment;
+#endif
+        }
+
         public string GetRootPath()
         {
+#if DNX451
+
+            return this.env.ApplicationBasePath;
+
+#else
             var assembly = Assembly.GetEntryAssembly();
 
-            return assembly != null ? 
+            return assembly != null ?
                 Path.GetDirectoryName(assembly.Location) :
                 Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+#endif
         }
     }
 }
--- src/Nancy/DefaultRootPathProvider.cs
+++ src/Nancy/DefaultRootPathProvider.cs
@@ -1,19 +1,35 @@
 ﻿namespace Nancy
 {
-    using System;
+#if DNX451
+    using Microsoft.Framework.Runtime;
+    using Microsoft.Framework.Runtime.Infrastructure;
+#endif
 
     /// <summary>
     /// Default implementation of <see cref="IRootPathProvider"/>.
     /// </summary>
     public class DefaultRootPathProvider : IRootPathProvider
     {
+        private readonly IApplicationEnvironment env;
+
+        public DefaultRootPathProvider()
+        {
+#if DNX451
+            this.env = CallContextServiceLocator.Locator.ServiceProvider.GetService(typeof(IApplicationEnvironment)) as IApplicationEnvironment;
+#endif
+
+        }
         /// <summary>
         /// Returns the root folder path of the current Nancy application.
         /// </summary>
         /// <returns>A <see cref="string"/> containing the path of the root folder.</returns>
         public string GetRootPath()
         {
+#if DNX451
+            return env.ApplicationBasePath;
+#else
             return AppDomain.CurrentDomain.BaseDirectory;
+#endif
         }
     }
 }
