a664340b122a0ecd6b49015d10b4678a0701dbe4
Christian Horsdal <c.horsdal@gmail.com>
2015-07-10 23:20:30 +0200

    add dnx hosting demo project which can run web listener or kestrel

---
+++ src/Nancy.Demo.Hosting.Dnx/Startup.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Nancy.Demo.Hosting.Dnx
+{
+    using Microsoft.AspNet.Builder;
+    using Microsoft.Framework.DependencyInjection;
+
+    using Nancy.Owin;
+
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseOwin().UseNancy();
+        }
+    }
+}
---
+++ src/Nancy.Demo.Hosting.Dnx/TestModule.cs
@@ -0,0 +1,10 @@
+﻿namespace Nancy.Demo.Hosting.Dnx
+{
+    public class TestModule : NancyModule
+    {
+        public TestModule()
+        {
+            Get["/"] = _ => "YO!";  
+        }
+    }
+}
